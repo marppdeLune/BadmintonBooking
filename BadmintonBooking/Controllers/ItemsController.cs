2080c97bf84d7502ddfc7dbeef9af1251a10aa60
Mike Mai <ext.mike.mai@languageconfidence.com>
2024-10-07 10:35:12 +1100

    Add project files.

---
+++ BadmintonBooking/Controllers/ItemsController.cs
@@ -0,0 +1,38 @@
+﻿using BadmintonBooking.Models;
+using Microsoft.AspNetCore.Mvc;
+using BadmintonBooking.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BadmintonBooking.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly BadmintonBookingContext _context;
+		public ItemsController(BadmintonBookingContext context)
+		{
+			_context = context;
+		}
+
+        public async Task<IActionResult> Index()
+        {
+            var item = await _context.Items.ToListAsync();
+            return View(item);
+        }
+        public IActionResult Create()
+		{
+			return View();
+		}
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id, Name")] Item item)
+        {
+            if(ModelState.IsValid)
+			{
+				_context.Items.Add(item);
+				await _context.SaveChangesAsync();
+				return RedirectToAction("Index");
+			}
+			return View(item);
+		}
+	}
+}
---
+++ BadmintonBooking/Data/BadmintonBookingContext.cs
@@ -0,0 +1,15 @@
+﻿using BadmintonBooking.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BadmintonBooking.Data
+{
+    public class BadmintonBookingContext : DbContext
+	{
+		public BadmintonBookingContext(DbContextOptions<BadmintonBookingContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Item> Items { get; set; }
+	}
+}
